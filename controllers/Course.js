a55356f3ca5f186827956f078b919c145c6833b4
Nazim Saifi <saifinazim2311@gmail.com>
2024-11-24 19:09:46 +0530

    added course controller

---
+++ controllers/Course.js
@@ -0,0 +1,75 @@
+const Category=require('../models/Category')
+const Course=require('../models/Course')
+const uploadFiles=require('../utils/uploadFiles')
+exports.createCourse=async(req,res)=>{
+    try {
+        const {courseName,courseDescription,price,whatYouWillLearn,category}=req.body
+        if(!courseName || !courseDescription || !price || !whatYouWillLearn || !category){
+            return res.status(411).json({
+                message:"All fields are required"
+            })
+        }
+        const thumbnail=req.files.thumbnail;
+        if(!thumbnail){
+            return res.status(403).json({
+                message:"Thumbnail not found"
+            })
+        }
+
+        const checkcategory=await Category.findOne({_id:category})
+        if(!checkcategory){
+            return res.status(404).json({
+                message:'Catgory is Invalid'
+            })
+        }
+        //upload on cloudinary
+        const fileresponse=uploadFiles(thumbnail,process.env.FOLDER_NAME,100);
+        console.log(fileresponse)
+
+        const newCourse=await Course.create({courseName,courseDescription,price,thumbnail:fileresponse.secure_url,instructor:req.user.id,category})
+
+        await Category.findOneAndUpdate({_id:category},{
+            $push:{
+                course:newCourse._id
+            }
+        })
+
+        await User.findOneAndUpdate({_id:req.user.id},{
+            $push:{
+                courses:newCourse._id
+            }
+        })
+        return res.status(200).json({
+            message:"Course created successfully",
+            newCourse
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(403).json({
+            message:"failed to create newcourse"
+        })
+    }
+}
+
+//getallcourse
+
+exports.getAllCourse=async(req,res)=>{
+    try {
+        const allCourses=await Course.find({},{courseName:true,
+                                                courseDescription:true,price:true,whatYouWillLearn:true,thumbnail:true,
+                                                instructor:true,
+                                                studentEnrolled:true
+                                                }).populate("instructor").exec()
+        return res.status(200).json({
+            message:"successfully fetched all Courses",
+            allCourses
+        })
+        
+    } catch (error) {
+         console.log(error)
+        return res.status(403).json({
+            message:"failed to fetched courses"
+        })
+    }
+}
