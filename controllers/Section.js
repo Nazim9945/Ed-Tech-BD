7a842684262b9f352f12eec3069f4b07a96ce2c5
Nazim Saifi <saifinazim2311@gmail.com>
2024-11-25 18:10:43 +0530

    added section controller

---
+++ controllers/Section.js
@@ -0,0 +1,88 @@
+//create
+
+const Course = require("../models/Course");
+const Section = require("../models/Section");
+
+exports.createSection=async(req,res)=>{
+    try {
+        const {courseId,sectionName}=req.body;
+       
+        const newSection=await Section.create({sectionName});
+
+        //update course
+
+        await Course.findOneAndUpdate({_id:courseId},{
+            $push:{
+                courseContent:newSection._id
+            }
+        })
+        return res.status(200).json({
+            message:"section created successfully",
+            newSection,
+
+        })
+
+    } catch (error) {
+        console.log(error)
+        return res.status(404).json({
+            success:false,
+            message:"Failed to create new section"
+        })
+    }
+}
+
+
+//update
+
+exports.sectionUpdate=async(req,res)=>{
+    try {
+        const {sectionName,sectionId}=req.body;
+        if(!sectionName || !sectionId){
+              return res.status(403).json({
+                message:"All fields are required"
+            })
+        }
+        await Section.findByIdAndUpdate({_id:sectionId},{sectionName});
+        return res.json({
+            message:"Section Name updated successfully"
+        })
+        
+    } catch (error) {
+         console.log(error)
+        return res.status(404).json({
+            success:false,
+            message:"Failed to update section"
+        })
+    }
+}
+
+
+//delete
+
+exports.sectionDelete=async(req,res)=>{
+    try {
+        const {courseId,sectionId}=req.body;
+        if(!courseId || sectionId){
+              return res.status(403).json({
+                message:"All fields are required"
+            })
+        }
+        await Section.findByIdAndDelete({_id:sectionId});
+
+        await Course.findByIdAndUpdate({_id:courseId},{
+            $pull:{
+                courseContent:sectionId
+            }
+        })
+        return res.json({
+            message:"Section Deleted successfully"
+        })
+        
+    } catch (error) {
+         console.log(error)
+        return res.status(404).json({
+            success:false,
+            message:"Failed to update section"
+        })
+    }
+}
